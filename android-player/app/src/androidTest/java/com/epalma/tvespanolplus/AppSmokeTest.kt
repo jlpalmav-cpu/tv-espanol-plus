b7e40d557132982174066d39d6e44315439e2b8f
jlpalmav-cpu <jlpalmav@gmail.com>
2026-09-15 23:41:35 -0600

    Add Android emulator smoke test

---
+++ android-player/app/src/androidTest/java/com/epalma/tvespanolplus/AppSmokeTest.kt
@@ -0,0 +1,18 @@
+package com.epalma.tvespanolplus
+
+import androidx.compose.ui.test.assertExists
+import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class AppSmokeTest {
+    @get:Rule val rule = createAndroidComposeRule<MainActivity>()
+
+    @Test fun appShowsBrandWithoutLogin() {
+        rule.onNode(hasContentDescription("TV Español+"), useUnmergedTree = true).assertExists()
+    }
+}
