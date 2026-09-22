d85ae1119559c4ceeaeef355777cd865d459459d
jlpalmav-cpu <jlpalmav@gmail.com>
2026-09-21 19:44:56 -0600

    Fix v1.6 lazy grid import

--- android-player/app/src/main/java/com/epalma/tvespanolplus/AdaptiveUiV7.kt
+++ android-player/app/src/main/java/com/epalma/tvespanolplus/AdaptiveUiV7.kt
@@ -31,7 +31,6 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.grid.item
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
